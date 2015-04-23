72ba903e8cfbd720dce1aae66d5a3c6257576b86
Dom Stockdale <dom.stockdale@gmail.com>
2015-04-23 18:01:20 +0700

    Changes languages and adds header

--- config/initializers/languages.rb
+++ config/initializers/languages.rb
@@ -1,2 +1,2 @@
-SpreeI18n::Config.available_locales = [:'zh-CN', :en] # displayed on translation forms
-SpreeI18n::Config.supported_locales = [:'zh-CN', :en] # displayed on frontend select box
+SpreeI18n::Config.available_locales = [:en, :fr, :de, :es, :pl] # displayed on translation forms
+SpreeI18n::Config.supported_locales = [:en, :fr, :de, :es, :pl] # displayed on frontend select box

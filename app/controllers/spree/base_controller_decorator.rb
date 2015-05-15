dcd0d0af2295d2c71a8accc1fbbb6baefa81fc16
Dom Stockdale <dom.stockdale@gmail.com>
2015-05-15 13:26:43 +0700

    Action that grabs taxonomies gobally as we always need them in the menu

---
+++ app/controllers/spree/base_controller_decorator.rb
@@ -0,0 +1,8 @@
+Spree::BaseController.class_eval do
+  before_filter :load_taxonomies
+
+  def load_taxonomies
+    @taxonomies = Spree::Taxonomy.includes(root: :children)
+  end
+
+end
--- app/controllers/spree/user_sessions_controller_decorator.rb
+++
@@ -1,8 +0,0 @@
-Spree::UserSessionsController.class_eval do
-  before_filter :load_taxons, :only => :new
-
-  def load_taxons
-    @taxonomies = Spree::Taxonomy.includes(root: :children)
-  end
-
-end

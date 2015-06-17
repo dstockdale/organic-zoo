79115925029884c82dc7cab6c552884cea1391ef
Dom Stockdale <doms.stockdale@gmail.com>
2015-06-17 18:06:17 +0700

    Adds refs to originals in deface overrides so changes are easier to debug

--- app/overrides/spree/spree_detail_layout.rb
+++ app/overrides/spree/spree_detail_layout.rb
@@ -2,33 +2,38 @@ Deface::Override.new(
   virtual_path: "spree/products/show",
   set_attributes: "[data-hook='product_left_part']",
   attributes: { class: "col-md-6" },
-  name: "spree_detail_layout_left"
+  name: "spree_detail_layout_left",
+  original: "b66161ed5e1326e5dc4798f1c224c26c41847c12"
 )
 
 Deface::Override.new(
   virtual_path: "spree/products/show",
   set_attributes: "[data-hook='product_right_part']",
   attributes: { class: "col-md-6" },
-  name: "spree_detail_layout_right"
+  name: "spree_detail_layout_right",
+  original: "9a3be2e75159e2ffc7197b844bb606694bb6475c"
 )
 
 Deface::Override.new(
   virtual_path: "spree/products/show",
   set_attributes: "#main-image",
   attributes: { class: "panel" },
-  name: "spree_detail_main_image"
+  name: "spree_detail_main_image",
+  original: "8765c76bd20d115a2cf166822487adce87c82c21"
 )
 
 Deface::Override.new(
   virtual_path: "spree/products/show",
   replace_contents: "#main-image .panel-body",
   partial: "spree/products/oz_image",
-  name: "spree_detail_img_responsive"
+  name: "spree_detail_img_responsive",
+  original: "86e85ec92bde47206b0ca1bc70fa3c6fc8067f99"
 )
 
 Deface::Override.new(
   virtual_path: "spree/products/show",
   replace_contents: "#product-description",
   partial: "spree/products/oz_description",
-  name: "spree_detail_oz_description"
+  name: "spree_detail_oz_description",
+  original: "c648a18cec686e1112ec0d3f49f72b0cd8aa4a5f"
 )
--- app/overrides/spree/spree_shared_products.rb
+++ app/overrides/spree/spree_shared_products.rb
@@ -2,5 +2,6 @@ Deface::Override.new(
   virtual_path: "spree/shared/_products",
   replace: "#products",
   partial: "spree/shared/oz_products",
-  name: "spree_shared_products"
+  name: "spree_shared_products",
+  original: '496f6c5295f928f57501b8eef8b19c39e63a8caf'
 )

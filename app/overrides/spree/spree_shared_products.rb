cc044df5f8a32527df199024cda350d0a093ceec
Dom Stockdale <doms.stockdale@gmail.com>
2015-08-06 11:06:20 +0100

    Moves .rb override into a deface one

--- app/overrides/spree/spree_shared_products.rb
+++
@@ -1,7 +0,0 @@
-Deface::Override.new(
-  virtual_path: "spree/shared/_products",
-  replace: "#products",
-  partial: "spree/shared/oz_products",
-  name: "spree_shared_products",
-  original: '496f6c5295f928f57501b8eef8b19c39e63a8caf'
-)

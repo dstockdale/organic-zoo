917b6768d5b16eb1ecf1ef86f5c7b1370a098507
Dom Stockdale <doms.stockdale@gmail.com>
2015-08-06 11:01:32 +0100

    Assets with S3 by default

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,24 +1,22 @@
-if Rails.env.production? then
-  attachment_config = {
+attachment_config = {
 
-    s3_credentials: {
-      access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
-      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      bucket:            ENV['AWS_S3_BUCKET']
-    },
+  s3_credentials: {
+    access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    bucket:            ENV['AWS_S3_BUCKET']
+  },
 
-    storage:        :s3,
-    s3_headers:     { "Cache-Control" => "max-age=31557600" },
-    s3_protocol:    "https",
-    bucket:         ENV['AWS_S3_BUCKET'],
-    url:            ":s3_domain_url",
+  storage:        :s3,
+  s3_headers:     { "Cache-Control" => "max-age=31557600" },
+  s3_protocol:    "https",
+  bucket:         ENV['AWS_S3_BUCKET'],
+  url:            ":s3_domain_url",
 
-    path:           "/spree/:class/:id/:style/:basename.:extension",
-    default_url:    "/spree/:class/:id/:style/:basename.:extension"
-  }
+  path:           "/spree/:class/:id/:style/:basename.:extension",
+  default_url:    "/spree/:class/:id/:style/:basename.:extension"
+}
 
-  attachment_config.each do |key, value|
-    Spree::Image.attachment_definitions[:attachment][key.to_sym] = value
-    Spree::Fairground::Slide.attachment_definitions[:attachment][key.to_sym] = value
-  end
+attachment_config.each do |key, value|
+  Spree::Image.attachment_definitions[:attachment][key.to_sym] = value
+  Spree::Fairground::Slide.attachment_definitions[:attachment][key.to_sym] = value
 end
